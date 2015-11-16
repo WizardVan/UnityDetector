bdd06032f02b386598477b3f2c80a511aeecf9e9
Wenzheng Xu <wx330@nyu.edu>
2015-11-16 16:27:11 +0800

    Add C# prototype
    Add C# prototype

---
+++ debugdetector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace debugdetector
+{
+    class Mydetector
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool CheckRemoteDebuggerPresent(IntPtr hProcess, ref bool isDebuggerPresent);
+        void Start()
+        {
+            //method 1: blacklist(sometimes evil thing is not only debugger)
+            Process[] localByName = Process.GetProcessesByName("OLLYDBG");
+            if (localByName != null && localByName.Length>0)
+            {
+                Console.Write("We are being fucked!!Ollydbg is so good\n");
+
+            }
+
+            //method 2: don't attach me (even you are a good debugger)
+            bool isDebuggerPresent = false;
+            CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref isDebuggerPresent);
+            if (isDebuggerPresent)
+            {
+                Console.WriteLine("Debugger Attached: " + isDebuggerPresent+"\n");
+                Console.ReadLine();
+            }
+            //method 3: it is what I said in 2 (this is for .net debugger XD)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("We are being fucked by .net debugger!!\n");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Thread.Sleep(10000);
+            Mydetector mydetector = new Mydetector();
+            mydetector.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ speedhackdetector/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Threading;
+
+namespace speedhackdetector
+{
+    class speedDetector
+    {
+        private const int THRESHOLD = 5000000;
+        private long ticksOnStart = 0;
+        private long ticksOnStartVulnerable = 0;
+        private int errorsCount=0;
+        private const int maxFalsePositives = 3;
+        public void Start()
+        {
+            errorsCount = 0;
+            ticksOnStart = DateTime.UtcNow.Ticks;
+            ticksOnStartVulnerable = Environment.TickCount * TimeSpan.TicksPerMillisecond;
+            Timer t = new Timer(OnTimer, null, 0, 2000);
+        }
+        private void OnTimer(Object o)
+        {
+            long ticks = 0;
+            long ticksVulnerable = 0;
+
+
+            ticks = DateTime.UtcNow.Ticks;
+            ticksVulnerable = Environment.TickCount * TimeSpan.TicksPerMillisecond;
+
+           // Console.WriteLine(ticksVulnerable - ticksOnStartVulnerable);
+            //Console.WriteLine(ticks - ticksOnStart);
+            if (Math.Abs((ticksVulnerable - ticksOnStartVulnerable) - (ticks - ticksOnStart)) > THRESHOLD)
+            {
+                errorsCount++;
+                if (errorsCount > maxFalsePositives)
+                {
+                    Console.WriteLine("Sry Baby,bad things happened!");
+                }
+            }
+            ticksOnStart = DateTime.UtcNow.Ticks;
+            ticksOnStartVulnerable = Environment.TickCount * TimeSpan.TicksPerMillisecond;
+        }
+        static void Main(string[] args)
+        {
+            speedDetector myspeedDetector = new speedDetector();
+            myspeedDetector.Start();
+            //Console.WriteLine("Pass maybe?");
+            //Console.ReadLine();
+        }
+    }
+}
